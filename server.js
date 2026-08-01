92d2f2c2057e1ee2dc9d55091163f760d4c0bd16
alecvipa <hebrit_626@hotmail.com>
2026-08-01 11:29:01 -0600

    added new

--- server.js
+++ server.js
@@ -204,6 +204,10 @@ app.get('/articles/inside-blue-shades', function (req, res) {
     var params = req.params.params;
     res.render('inside-blue-shades');
 });
+app.get('/articles/vilpa-influencial-actors', function (req, res) {
+    var params = req.params.params;
+    res.render('vilpa-influencial-actors');
+});
 app.get('/terms', function (req, res) {
     var params = req.params.params;
     res.render('terms');
